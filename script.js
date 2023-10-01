57f5574ee214ebe13f33651d8872468b6b51522b
poswalsameer <poswalsameer2003@gmail.com>
2023-10-01 19:26:54 +0530

    Added the feature of play on clicking the div

--- script.js
+++ script.js
@@ -16,10 +16,16 @@ let songList = [
 
 ]
 
-let music = new Audio('OG.mp3');
+let music = new Audio('Cheques.mp3');
 let playBtn = document.getElementById('playBtn');
 let audioProgress = document.getElementById('bar');
 // music.play();
+// let chequeSong = new Audio('Cheques.mp3');
+let og = document.getElementById('og');
+let wakeUp = document.getElementById('wakeUp');
+let pind = document.getElementById('pind');
+let levels = document.getElementById('levels');
+let obsessed = document.getElementById('obsessed');
 
 
 //Playing and pausing the song
@@ -32,6 +38,46 @@ playBtn.addEventListener('click', ()=>{
         music.pause();
         document.getElementById('play').src = 'play.png';
     }
+})
+
+og.addEventListener('click', ()=> {
+
+    music.src = 'OG.mp3';
+    music.play();
+    document.getElementById('play').src = 'pause.png'; 
+
+})
+
+wakeUp.addEventListener('click', ()=> {
+
+    music.src = 'Wake Up.mp3';
+    music.play();
+    document.getElementById('play').src = 'pause.png'; 
+
+})
+
+pind.addEventListener('click', ()=> {
+
+    music.src = 'Pind Da Riwaaz.mp3';
+    music.play();
+    document.getElementById('play').src = 'pause.png'; 
+
+})
+
+levels.addEventListener('click', ()=> {
+
+    music.src = 'Levels (MrJatt.Im).mp3';
+    music.play();
+    document.getElementById('play').src = 'pause.png'; 
+
+})
+
+obsessed.addEventListener('click', ()=> {
+
+    music.src = 'Obsessed - 320Kbps-(Mr-Jat.in).mp3';
+    music.play();
+    document.getElementById('play').src = 'pause.png'; 
+
 })
 
 music.addEventListener('timeupdate', ()=> {
