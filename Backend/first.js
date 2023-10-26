a7721018f53bfb0961f8827f337abb74ddbab9c5
poswalsameer <poswalsameer2003@gmail.com>
2023-10-26 20:10:35 +0530

    Added 2 new songs now next step is to work on javascript

---
+++ Backend/first.js
@@ -0,0 +1,22 @@
+// HOW TO START SERVER IN TERMIAL
+
+// Type node + folder name + \ + file name
+
+const http = require('http');
+ 
+const hostname = '127.0.0.1';
+const port = 3000;
+ 
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello sameer, you have made your first server');
+//   res.setHeader('Content-Type', 'text/html');
+//   res.end(`Paste your html file here`);
+});
+ 
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
