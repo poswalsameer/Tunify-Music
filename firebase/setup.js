61130a3968ef6b61e9df36b36bfbf014527544c2
poswalsameer <poswalsameer2003@gmail.com>
2023-11-05 19:18:21 +0530

    setup of collection and document and printing them on the console

--- firebase/setup.js
+++ firebase/setup.js
@@ -15,6 +15,9 @@
 // console.log("Hello, this is me who is using firebase for the first time");
 
 import { initializeApp } from 'firebase/app';
+import {
+    getFirestore, collection, getDocs, getDoc
+} from 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyAzcU3kufMN0gf-vjuU6aTbnnG29hkMMk4",
@@ -27,3 +30,30 @@ const firebaseConfig = {
 
 //this below written code initialized the firebase app
 initializeApp(firebaseConfig);
+
+//creating a new database using the firestore method
+const db = getFirestore();
+
+//collecting reference to a collection made in firebase
+const colRef = collection(db, 'User Details');
+
+//getting the documents from the collection
+getDocs(colRef)
+  .then( (snapshot) => {
+        // console.log(snapshot.docs);
+
+        //this array is created to show all the docs in a better manner in the console
+        let userDetails = []
+
+        //this loop will make sure that the array is shown in a well presented manner in the console
+        snapshot.docs.forEach((doc) => {
+            userDetails.push( { ...doc.data(), id: doc.id } )
+        } )
+
+        //console to show (not necessary)
+        console.log(userDetails);
+  } )
+  .catch( err => {
+        //catch method to catch any kind of error if present in the code
+        console.log("There is some error in your code!");
+  } )
